38595701ef05a4bd4c785c6550eb1d85e03adf20
ariellouise <arielladders@gmail.com>
2023-04-17 14:37:20 -0400

    updated app.js
    added array

---
+++ app.js
@@ -0,0 +1,34 @@
+const cardArray = [
+  {
+    name: "gyoza",
+    img: "images/gyoza.jpg",
+  },
+  {
+    name: "frenchtoast",
+    img: "images/frenchtoast.jpg",
+  },
+  {
+    name: "bagelsandwich",
+    img: "images/bagelsandwich.jpg",
+  },
+  {
+    name: "cookies",
+    img: "images/cookies.jpg",
+  },
+  {
+    name: "pasta",
+    img: "images/pasta.jpg",
+  },
+  {
+    name: "sushi",
+    img: "images/sushi.jpg",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.jpg",
+  },
+  {
+    name: "cupcakes",
+    img: "images/cupcakes.jpg",
+  },
+];
