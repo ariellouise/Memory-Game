53656ca55e761a5edfe77c5fc214cdb8f21cff74
ariellouise <arielladders@gmail.com>
2023-04-17 14:47:59 -0400

    updated app.js

--- app.js
+++ app.js
@@ -31,4 +31,40 @@ const cardArray = [
     name: "cupcakes",
     img: "images/cupcakes.jpg",
   },
+  {
+    name: "gyoza",
+    img: "images/gyoza.jpg",
+  },
+  {
+    name: "frenchtoast",
+    img: "images/frenchtoast.jpg",
+  },
+  {
+    name: "bagelsandwich",
+    img: "images/bagelsandwich.jpg",
+  },
+  {
+    name: "cookies",
+    img: "images/cookies.jpg",
+  },
+  {
+    name: "pasta",
+    img: "images/pasta.jpg",
+  },
+  {
+    name: "sushi",
+    img: "images/sushi.jpg",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.jpg",
+  },
+  {
+    name: "cupcakes",
+    img: "images/cupcakes.jpg",
+  },
 ];
+
+cardArray.sort(() => 0.5 - Math.random());
+
+document.querySelector("#grid");
