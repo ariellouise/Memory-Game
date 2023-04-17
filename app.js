39e3ac135291bd29d0bf0949afb8fb61549c36d0
ariellouise <arielladders@gmail.com>
2023-04-17 16:24:03 -0400

    updated images
    updated images, css, app.js

--- app.js
+++ app.js
@@ -66,5 +66,20 @@ const cardArray = [
 ];
 
 cardArray.sort(() => 0.5 - Math.random());
+//console.log(cardArray);
 
-document.querySelector("#grid");
+const gridDisplay = document.querySelector("#grid");
+
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/background.jpg");
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
+    gridDisplay.append(card);
+  }
+}
+
+createBoard();
+
+function flipCard() {}
