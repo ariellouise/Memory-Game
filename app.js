dfe331dd3d93be592eadeae084812e1ad1a7fd0c
ariellouise <arielladders@gmail.com>
2023-04-17 20:29:22 -0400

    updated app.js
    innerHTML

--- app.js
+++ app.js
@@ -69,8 +69,10 @@ cardArray.sort(() => 0.5 - Math.random());
 //console.log(cardArray);
 
 const gridDisplay = document.querySelector("#grid");
-const cardsChosen = [];
-const cardsChosenIds = [];
+const resultDisplay = document.querySelector("#result");
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
 
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++) {
@@ -86,12 +88,29 @@ createBoard();
 
 function checkMatch() {
   const cards = document.querySelectorAll("img");
-  //console.log("check for match!");
+  const optionOneId = cardsChosenIds[0];
+  const optionTwoId = cardsChosenIds[1];
+  if (optionOneId == optionTwoId) {
+    alert("You have clicked the same image!");
+  }
   if (cardsChosen[0] == cardsChosen[1]) {
     alert("You found a match!");
-    cards[cardsChosenIds[0]].setAttribute("src", "images/white.png");
-    cards[cardsChosenIds[1]].setAttribute("src", "images/white.png");
-    cards[cardsChosenIds[0]].removeEventListener("click", flipCard);
+    cards[optionOneId].setAttribute("src", "images/white.png");
+    cards[optionTwoId].setAttribute("src", "images/white.png");
+    cards[optionOneId].removeEventListener("click", flipCard);
+    cards[optionTwoId].removeEventListener("click", flipCard);
+    cardsWon.push(cardsChosen);
+  } else {
+    cards[optionOneId].setAttribute("src", "images/background.jpg");
+    cards[optionTwoId].setAttribute("src", "images/background.jpg");
+    alert("Sorry! Try again!");
+  }
+
+  cardsChosen = [];
+  cardsChosenIds = [];
+
+  if (cardsWon.length == cardArray.length / 2) {
+    resultDisplay.innerHTML = "Congrats! You matched all the pairs!";
   }
 }
 
@@ -102,6 +121,6 @@ function flipCard() {
   cardsChosenIds.push(cardId);
   this.setAttribute("src", cardArray[cardId].img);
   if (cardsChosen.length === 2) {
-    setTimeout(checkMatch, 500);
+    setTimeout(checkMatch, 100);
   }
 }
